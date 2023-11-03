8828e83960da7fe1d2f7819c1e604a5e8edffd6e
alsolus <tefamborambauri01@gmail.com>
2023-11-03 14:52:08 +0700

    -

--- update.php
+++ update.php
@@ -8,7 +8,7 @@ $nama = $_POST['nama'];
 $jenis_kelamin = $_POST['jenis_kelamin'];
 
 // menginput data ke database
-$db->query("UPDATE kel_budi SET nama='$nama', jenis_kelamin='$jenis_kelamin', WHERE id='$id'");
+$db->query("UPDATE kel_budi SET nama='$nama', jenis_kelamin='$jenis_kelamin' WHERE id='$id'");
 
 // mengalihkan halaman kembali ke index.php
 header("location:index.php?pesan=update");
